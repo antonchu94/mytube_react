9557df9ea2bb8cb57f2ea0102e26d6806425251a
antonchu94 <antonchurikov94@gmail.com>
2022-10-19 15:23:51 +0300

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import TvIcon from '@mui/icons-material/Tv';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/wKXW50p/ukraine-flag-42040.png';
+
+/*https://i.ibb.co/fMYcMn7/ukraine-flag-42041.png
+https://i.ibb.co/wKXW50p/ukraine-flag-42040.png*/
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'Material UI', icon: <CodeIcon />, },
+  { name: 'Anton Ptushkin', icon: <TravelExploreIcon /> },
+  { name: 'SetantaSport', icon: <FitnessCenterIcon />, },
+  { name: 'Maxim Katz', icon: <BusinessCenterIcon />, },
+  { name: 'CNN', icon: <TvIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, }, 
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const myThumbnailUrl = 'https://i.ibb.co/fMYcMn7/ukraine-flag-42041.png';
+export const myChannelUrl = '/channel/UCUGfDbfRIx51kJGGHIFo8Rw';
+export const myVideoUrl = '/video/Vw00Vpf-Fdo&t=581s';
+export const myChannelTitle = 'MyTubeUA install @mui/material @emotion/react @emotion/styled';
+export const myVideoTitle = 'MyTubeUA';
+export const myProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
